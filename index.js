105c246ebb9f27add6ec8898dd1ddd48b882c0fd
Nafyad <99583059+SnollyGhost@users.noreply.github.com>
2024-05-08 15:46:32 +0300

    Update index.js
    - allow the user to take a screenshot or download the receipt for 30 sec...
    - Changed .then() and .catch() to async/await for cleaner asynchronous code...
    - Added error handling for asynchronous operations..
    - Used res.sendStatus() for sending HTTP status codes in response...

--- index.js
+++ index.js
@@ -1,74 +1,71 @@
-/* 
-    CHAPA API PAYMENT INTEGRATION TEST
-    Required: Chapa secret key || GET THE KEY BY REGISTERING @ https://dashboard.chapa.co/register
-*/
 
-const express = require("express")
-const app = express()
-const axios = require("axios").default
-require("dotenv").config()
-const PORT = process.env.PORT || 4400
 
-const CHAPA_URL = process.env.CHAPA_URL || "https://api.chapa.co/v1/transaction/initialize"
+const express = require("express");
+const app = express();
+const axios = require("axios").default;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4400;
+const CHAPA_URL =
+  process.env.CHAPA_URL || "https://api.chapa.co/v1/transaction/initialize";
 const CHAPA_AUTH = process.env.CHAPA_AUTH // || register to chapa and get the key
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-// req header with chapa secret key
 const config = {
-    headers: {
-        Authorization: `Bearer ${CHAPA_AUTH}`
-    }
-}
+  headers: {
+    Authorization: `Bearer ${CHAPA_AUTH}`,
+  },
+};
 
-// entry for the front end
-app.get('/', (req, res) => {
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-// initial payment endpoint
 app.post("/api/pay", async (req, res) => {
+  const CALLBACK_URL = `http://localhost:${PORT}/api/verify-payment/`;
+  const RETURN_URL = `http://localhost:${PORT}/api/payment-success/`;
+  const TEXT_REF = "tx-myecommerce12345-" + Date.now();
+  const data = {
+    amount: "100",
+    currency: "ETB",
+    email: "ato@ekele.com",
+    first_name: "Ato",
+    last_name: "Ekele",
+    tx_ref: TEXT_REF,
+    callback_url: CALLBACK_URL + TEXT_REF,
+    return_url: RETURN_URL,
+  };
 
-         // chapa redirect you to this url when payment is successful
-        const CALLBACK_URL = "http://localhost:4400/api/verify-payment/"
-        const RETURN_URL = "http://localhost:4400/api/payment-success/"
+  try {
+    const response = await axios.post(CHAPA_URL, data, config);
 
-        // a unique reference given to every transaction
-        const TEXT_REF = "tx-myecommerce12345-" + Date.now()
 
-        // form data
-        const data = {
-            amount: '100', 
-            currency: 'ETB',
-            email: 'ato@ekele.com',
-            first_name: 'Ato',
-            last_name: 'Ekele',
-            tx_ref: TEXT_REF,
-            callback_url: CALLBACK_URL + TEXT_REF,
-            return_url: RETURN_URL
-        }
+    res.redirect(response.data.data.checkout_url);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-        // post request to chapa
-        await axios.post(CHAPA_URL, data, config)
-            .then((response) => {
-                res.redirect(response.data.data.checkout_url)
-            })
-            .catch((err) => console.log(err))
-})
-
-// verification endpoint
 app.get("/api/verify-payment/:id", async (req, res) => {
-    
-        //verify the transaction 
-        await axios.get("https://api.chapa.co/v1/transaction/verify/" + req.params.id, config)
-            .then((response) => {
-                console.log("Payment was successfully verified")
-            }) 
-            .catch((err) => console.log("Payment can't be verfied", err))
-})
+  try {
+    await axios.get(
+      `https://api.chapa.co/v1/transaction/verify/${req.params.id}`,
+      config
+    );
+    console.log("Payment was successfully verified");
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Payment can't be verified", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-app.get("/api/payment-success", async (req, res) => {
-    res.render("success")
-})
-
-app.listen(PORT, () => console.log("Server listening on port:", PORT))
+app.get("/api/payment-success", (req, res) => {
+  // allow the user to take a screenshot or download the RECIEPT
+  setTimeout(() => {
+    res.render("success");
+  }, 30000); // 30 seconds
+});
+app.listen(PORT, () => console.log("Server listening on port:", PORT));
